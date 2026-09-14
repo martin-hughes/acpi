d4b3c42c936130a31aba2f86f40358de61b48a6d
Martin Hughes <martin-hughes@users.noreply.github.com>
2026-09-14 12:05:07 +0100

    Prevent some infinite recursion and loops
    The motivation is to help stop AML causing a thread (or the system as a
    whole) to become deadlocked.
    
    The first protection adds a maximum method call stack depth. This has
    been chosen arbitrarily, for now. It does not protect against all
    possible ways of recursing infinitely. For example, it does not stop
    recursive table loads. (Although LoadTable is not currently supported)
    
    The second protection is a straightforward timeout for while loops,
    with associated tests.
    
    These are both based on the uACPI protections, although they differ in
    a key way: in uACPI, the method stack is unwound. If a table definition
    is reached, execution continues with the next statement, but the
    execution is still flagged as failed. In this crate, execution is
    stopped with an error.

--- tools/aml-test-tools/src/lib.rs
+++ tools/aml-test-tools/src/lib.rs
@@ -24,7 +24,7 @@ use log::{error, trace};
 use std::{
     cell::SyncUnsafeCell,
     ffi::OsStr,
-    fmt::Debug,
+    fmt::{Debug, Formatter},
     fs::File,
     io::{Read, Write},
     panic::{AssertUnwindSafe, catch_unwind},
@@ -50,6 +50,19 @@ where
 
     /// The test failed, and the interpreter is no longer valid.
     Panicked,
+}
+
+impl<T> std::fmt::Debug for RunTestResult<T>
+where
+    T: Handler,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            RunTestResult::Pass(_) => "Pass",
+            RunTestResult::Failed(_, _) => "Failed",
+            RunTestResult::Panicked => "Panicked",
+        })
+    }
 }
 
 /// The result of a test
